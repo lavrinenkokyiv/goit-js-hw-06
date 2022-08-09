e118e1876c084b941b6038d80827d83c30804b10
lavrinenkokyiv <88554016+lavrinenkokyiv@users.noreply.github.com>
2022-08-09 19:17:19 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const changeColorBtn = document.querySelector('.change-color')
+const colorValue = document.querySelector('.color')
+const bodyEl = document.querySelector('body')
+
+
+colorValue.textContent = '#ffffff'
+changeColorBtn.addEventListener('click', onChangeColorBtn)
+function onChangeColorBtn(event) {
+  event.preventDefault()
+const newColor = getRandomHexColor()
+  bodyEl.style.backgroundColor = newColor
+  colorValue.innerHTML = `${newColor}`
+  console.log(newColor);
 }
