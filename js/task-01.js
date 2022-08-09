39844c91dd4e2ae09508c4018ec252a34474cf4b
lavrinenkokyiv <88554016+lavrinenkokyiv@users.noreply.github.com>
2022-08-09 19:16:46 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const listEl = document.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${listEl.length}.`);
+
+const ulEl = document.querySelectorAll('#categories>li');
+ulEl.forEach(el => {
+console.log(
+    `Category: ${el.firstElementChild.textContent}
+     Elements: ${el.lastElementChild.children.length}`,
+  );
+});
