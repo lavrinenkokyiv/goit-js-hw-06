8d93aba4513aff2d56462b975eb5e49e01d71555
lavrinenkokyiv <88554016+lavrinenkokyiv@users.noreply.github.com>
2022-08-09 19:16:48 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients');
+
+const elementRef = ingredients.map(element => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = element;
+  return itemRef;
+}
+  )
+  ulEl.append(...elementRef)
