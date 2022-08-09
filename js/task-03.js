35bfe83605ceb37be780fdcf49553b2652d6a51d
lavrinenkokyiv <88554016+lavrinenkokyiv@users.noreply.github.com>
2022-08-09 19:16:50 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryEl = document.querySelector('.gallery');
+
+const markupGallery = images
+  .map(({ url, alt }) => 
+  `<li class='gallery__item'><img src="${url}" alt="${alt}"></li>`)
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markupGallery);
