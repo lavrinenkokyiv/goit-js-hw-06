41b9268dd04cc41e7180aa390d43b678212a47ee
lavrinenkokyiv <88554016+lavrinenkokyiv@users.noreply.github.com>
2022-08-12 16:38:52 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,16 @@
-const loginFormEl = document.querySelector('form.login-form')
-const emailEl = loginFormEl.querySelector('[type = "email"]')
-const passwordEl = loginFormEl.querySelector('[type = "password"]')
+const FormEl = document.querySelector(".login-form");
 
-loginFormEl.addEventListener('submit', setOutput)
-function setOutput(evt) {
-    evt.preventDefault();
-    const {
-        elements: {email, password},
-    } = evt.currentTarget;
-    if (email.value === '' || password.value === '') {
-        return alert ('Заповніть всі поля');
+const submitForm = (event) => {
+    const emailEl = event.target.elements.email.value;
+    const passwordEl = event.target.elements.password.value;
+    const object = { emailEl, passwordEl,};   
+    
+    event.preventDefault();
+    if (emailEl === "" || passwordEl === "") {
+    return alert("Заповніть всі поля");
     }
-    console.log(`Email: ${email.value}, Password: ${password.value}`);
-    evt.currentTarget.reset();}
+    console.log(object);
+    return FormEl.reset();
+};
+
+FormEl.addEventListener("submit", submitForm);
